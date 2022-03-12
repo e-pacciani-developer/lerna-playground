89f416eacd00eff7600d5f750f8f4429e35143fa
Edoardo Pacciani <e.pacciani.developer@gmail.com>
2022-03-12 13:17:56 +0100

    ll

--- packages/app/src/components/Layout/index.tsx
+++ packages/app/src/components/Layout/index.tsx
@@ -1,10 +1,10 @@
 import { Container, Flex } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
-import Header from '../Header';
+import { Header } from '@isendu/components';
 const Layout: React.FC = () => {
   return (
     <Container minH={'100vh'} bg="gray.200" minW={'100vw'} padding="0">
-      <Header />
+      <Header myText="ciao ciao" />
       <Flex justify="center" height={'calc(100vh - 80px)'}>
         <Outlet />
       </Flex>
